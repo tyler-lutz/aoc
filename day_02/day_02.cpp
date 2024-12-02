fd9a104947c6618f971bc60f8b8c1250d2a650ad
Tyler Lutz <180100824+tyler-lutz@users.noreply.github.com>
2024-12-02 16:52:41 -0500

    feat: day 2 part 2

--- day_02/day_02.cpp
+++ day_02/day_02.cpp
@@ -20,6 +20,17 @@ bool is_safe(std::vector<int> levels) {
   return increasing || decreasing;
 }
 
+bool problem_dampener(std::vector<int> levels) {
+  for (int i = 0; i < levels.size(); i++) {
+    std::vector<int> modified = levels;
+    modified.erase(modified.begin() + i);
+
+    if (is_safe(modified))
+      return true;
+  }
+  return false;
+}
+
 int main() {
   std::ifstream input_file("input");
 
@@ -45,15 +56,25 @@ int main() {
 
   input_file.close();
 
-  int safe_count = 0;
+  int part_1 = 0;
 
   for (std::vector<int> report : reports) {
     bool safe = is_safe(report);
     if (safe)
-      safe_count++;
+      part_1++;
   }
 
-  std::cout << "Part 1: " << safe_count << std::endl;
+  std::cout << "Part 1: " << part_1 << std::endl;
+
+  int part_2 = 0;
+
+  for (std::vector<int> report : reports) {
+    bool safe = is_safe(report) || problem_dampener(report);
+    if (safe)
+      part_2++;
+  }
+
+  std::cout << "Part 2: " << part_2 << std::endl;
 
   return 0;
 }
